37c72a7241db9aff41c96a979a1fe24216a58425
Chris Tran <60266790+trannchris@users.noreply.github.com>
2023-10-06 04:12:46 -0500

    Added replay button

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let games = 0;
 let wins = 0;
 let losses = 0;
-let draws = 0;
 
 const buttons = document.querySelectorAll("button");
+const instructions = document.querySelector(".instructions");
+const resultText = document.querySelector(".resultText");
+const roundText = document.querySelector(".roundText");
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     playRound(button.id, getComputerChoice());
+    checkScore();
   });
 });
 
@@ -25,27 +27,50 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-  playerString =
+  const playerString =
     playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
-  computerString =
+  const computerString =
     computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
 
-  resultText = document.querySelector(".resultText");
-
   if (playerSelection == computerSelection) {
-    draws++;
-    resultText.textContent = "Draw!";
+    roundText.textContent = "Draw!";
   } else if (
     (playerSelection == "paper" && computerSelection == "rock") ||
     (playerSelection == "rock" && computerSelection == "scissors") ||
     (playerSelection == "scissors" && computerSelection == "paper")
   ) {
     wins++;
-    resultText.textContent =
+    roundText.textContent =
       "You Win! " + playerString + " beats " + computerString;
   } else {
     losses++;
-    resultText.textContent =
+    roundText.textContent =
       "You Lose! " + computerString + " beats " + playerString;
+  }
+
+  instructions.textContent = `Wins: ${wins} Losses: ${losses}`;
+}
+
+function checkScore() {
+  if (wins == 5 || losses == 5) {
+    roundText.style.display = "none";
+    restartButton = document.createElement("button");
+    restartButton.textContent = "Play again";
+    restartButton.addEventListener("click", () => {
+      wins = 0;
+      losses = 0;
+      instructions.textContent =
+        "You are being challenged by a nobody! Play rock, paper, scissors. First to 5 wins gets to have bragging rights. Begin!";
+      resultText.removeChild(restartButton);
+      roundText.style.display = "inline";
+      roundText.textContent = "";
+    });
+
+    resultText.appendChild(restartButton);
+  }
+  if (wins == 5) {
+    instructions.textContent = "Congratulations, you have won!";
+  } else if (losses == 5) {
+    instructions.textContent = "You lost! Now you officially suck.";
   }
 }
